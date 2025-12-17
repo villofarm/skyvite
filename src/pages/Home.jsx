6a9931cced444fdaf64013b5b672f71e4118737c
villofarm <villofarmnatural@gmail.com>
2025-12-18 00:18:05 +0530

    Fix fourcard h2 color targeting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import aboutimg from "../assets/img/about-dashboard.webp";
+import firstcard from "../assets/img/school.png";
 
 export default function Home() {
   return (
@@ -38,21 +39,69 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="fourcardsec py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="fourcard">
+                <img src={firstcard} />
+                <div>
+                  <h2>300+</h2>
+                  <p>Educational Institutions</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="fourcard">
+                <img src={firstcard} />
+                <div>
+                  <h2>1M+</h2>
+                  <p>Students Managed</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="fourcard">
+                <img src={firstcard} />
+                <div>
+                  <h2>99.9%</h2>
+                  <p>Uptime Guarantee</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="fourcard">
+                <img src={firstcard} />
+                <div>
+                  <h2>24/7</h2>
+                  <p>Customer Support</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="about-section position-relative">
         <div className="container">
-            <div className="row">
-              <div className="col-md-6 ">
-                <span className="badge">Seamless Experience</span>
-                <h2>Manage Your Institution <br></br><span>On the Go</span></h2>
-                <p>Skyvite is a modern, AI-powered student management software designed to help schools operate smarter and faster. </p>
-              </div>
-              <div className="col-md-6 position-relative">
-                <img className="img-fluid dshboard w-100" src={aboutimg} alt="" />
-              </div>
-              {/* <div className="col-12 p-0 gradientbg">
+          <div className="row">
+            <div className="col-md-6 ">
+              <span className="badge">Seamless Experience</span>
+              <h2>
+                Manage Your Institution <br></br>
+                <span>On the Go</span>
+              </h2>
+              <p>
+                Skyvite is a modern, AI-powered student management software
+                designed to help schools operate smarter and faster.{" "}
+              </p>
+            </div>
+            <div className="col-md-6 position-relative">
+              <img className="img-fluid dshboard w-100" src={aboutimg} alt="" />
+            </div>
+            {/* <div className="col-12 p-0 gradientbg">
                 <img decoding="auto" width="1400" height="513" sizes="1500px" srcset="https://framerusercontent.com/images/AwrbWiEWUy4IouNnstLMWwwifU.webp?scale-down-to=512&amp;width=1400&amp;height=513 512w,https://framerusercontent.com/images/AwrbWiEWUy4IouNnstLMWwwifU.webp?scale-down-to=1024&amp;width=1400&amp;height=513 1024w,https://framerusercontent.com/images/AwrbWiEWUy4IouNnstLMWwwifU.webp?width=1400&amp;height=513 1400w" src="https://framerusercontent.com/images/AwrbWiEWUy4IouNnstLMWwwifU.webp?width=1400&amp;height=513" alt="" />
               </div> */}
-            </div>
+          </div>
         </div>
       </section>
     </div>
